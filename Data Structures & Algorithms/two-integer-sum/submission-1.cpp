cf520fd368768adce53b2a69f934082ade3d40ff
Marcus D. Duer <marcus0duer@gmail.com>
2026-08-22 18:14:16 +0200

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i{}; i < nums.size(); ++i)
+        {
+            for (int j{i + 1}; j < nums.size(); ++j) 
+            {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
