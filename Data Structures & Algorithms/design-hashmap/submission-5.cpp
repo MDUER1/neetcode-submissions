05396ad8e7cd53584ff390b70f7ded43a60b6a11
Marcus D. Duer <marcus0duer@gmail.com>
2026-08-24 10:36:03 +0200

    Add: design-hashmap - submission-5

---
+++ Data Structures & Algorithms/design-hashmap/submission-5.cpp
@@ -0,0 +1,21 @@
+class MyHashMap {
+private:
+    vector<int> data;
+public:
+    MyHashMap() : data(10000001, -1) {}
+
+    void put(int key, int value) {
+        data[key] = value;
+    }
+
+    int get(int key) {
+        return data[key];
+    }
+
+    void remove(int key) {
+        if (data[key] != -1) {
+            data[key] = -1;
+        }
+    }
+
+};
